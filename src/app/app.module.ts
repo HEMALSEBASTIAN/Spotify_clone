95550f3826beeebea57fe1fe31f564933e6184b4
Hemal Sebastian <hemals@GMG.LOCAL>
2023-09-13 23:03:12 +0530

    Body added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderContainerComponent } from './header-container/header-container.component';
 import { BodyContainerComponent } from './body-container/body-container.component';
 import { FooterContainerComponent } from './footer-container/footer-container.component';
+import { BodyContainerModule } from './body-container/body-container.module';
+import { GridContainerComponent } from './body-container/grid-container/grid-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderContainerComponent,
-    BodyContainerComponent,
-    FooterContainerComponent
+    FooterContainerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BodyContainerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
