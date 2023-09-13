31c15bdd3aaed0eda017fcff2d48c121817a86be
Hemal Sebastian <hemals@GMG.LOCAL>
2023-09-13 21:03:42 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderContainerComponent } from './header-container/header-container.component';
+import { BodyContainerComponent } from './body-container/body-container.component';
+import { FooterContainerComponent } from './footer-container/footer-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderContainerComponent,
+    BodyContainerComponent,
+    FooterContainerComponent
   ],
   imports: [
     BrowserModule,
