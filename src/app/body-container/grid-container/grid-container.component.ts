4f50d4f8f399346175f15d6c18f70b681566adcf
HEMALSEBASTIAN <hemalsebastian123@gmail.com>
2023-09-16 15:39:45 +0530

    edit tab added

--- src/app/body-container/grid-container/grid-container.component.ts
+++ src/app/body-container/grid-container/grid-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IMusic } from 'src/app/app-component.interface';
 import { MusicServiceService } from 'src/app/services/music-service.service';
 
@@ -7,13 +8,19 @@ import { MusicServiceService } from 'src/app/services/music-service.service';
   templateUrl: './grid-container.component.html',
   styleUrls: ['./grid-container.component.scss']
 })
+
 export class GridContainerComponent implements OnInit{
-  musicList!:Array<IMusic>;
-  constructor(private readonly musicService:MusicServiceService){}
-  ngOnInit(): void {
+  public musicList!:Array<IMusic>;
+  constructor(private readonly musicService:MusicServiceService,
+    private readonly router:Router){}
+  public ngOnInit(): void {
     this.musicList=this.musicService.getMusics();
   }
 
+  public edit(musicId?: number)
+  {
+    this.router.navigate(['createMusic'], {queryParams: {musicId: musicId}});
+  }
 }
 
 
--- src/app/body-container/music-creation/music-creation.component.ts
+++ src/app/body-container/music-creation/music-creation.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LastValueFromConfig } from 'rxjs/internal/lastValueFrom';
 import { IMusic } from 'src/app/app-component.interface';
 import { MusicServiceService } from 'src/app/services/music-service.service';
 
@@ -8,30 +10,111 @@ import { MusicServiceService } from 'src/app/services/music-service.service';
   templateUrl: './music-creation.component.html',
   styleUrls: ['./music-creation.component.scss']
 })
-export class MusicCreationComponent implements OnInit{
-  public music! : Partial<IMusic>;
+export class MusicCreationComponent implements OnInit {
+  public music!: Partial<IMusic>;
 
-  constructor(private readonly router:Router, private readonly musicService :MusicServiceService){}
+  public musicProduct!: FormGroup<{
+    id: FormControl<any>,
+    albumName: FormControl<any>,
+    authorName: FormControl<any>,
+    category: FormControl<any>,
+    imageUrl: FormControl<any>
+  }>;
 
-  ngOnInit(): void {
-    this.music={
-      albumName:"",
-      authorName:"",
-      category:"",
-      imageUrl:""
+  public authorNameCheck!: AbstractControl;
+  public categoryCheck!: AbstractControl;
+  public imageUrlCheck!: AbstractControl;
+
+  public validationCheck: boolean = false;
+  public title: string = '';
+  public musicId?: number;
+  public currentMusic!: IMusic;
+
+  constructor(private readonly router: Router,
+    private readonly musicService: MusicServiceService,
+    private readonly fb: FormBuilder,
+    private readonly cdr: ChangeDetectorRef,
+    private readonly route: ActivatedRoute) { }
+
+  public ngOnInit(): void {
+    this.musicProduct = this.fb.group({
+      id: [undefined],
+      albumName: ['', Validators.required],
+      authorName: ['', Validators.required],
+      category: ['', Validators.required],
+      imageUrl: ['', Validators.required]
+    })
+
+    this.authorNameCheck = this.musicProduct.controls['authorName'];
+    this.categoryCheck = this.musicProduct.controls['category'];
+    this.imageUrlCheck = this.musicProduct.controls['imageUrl'];
+
+    this.route.queryParams.subscribe(params => {
+      this.musicId = params['musicId']
+    });
+    console.log(this.musicId);
+    if (this.musicId != null) {
+      this.currentMusic = this.musicService.getMusicById(this.musicId) as IMusic;
+      console.log(this.currentMusic);
+      this.musicProduct = this.fb.group({
+        id: [this.currentMusic.id, Validators.required],
+        albumName: [this.currentMusic.albumName, Validators.required],
+        authorName: [this.currentMusic.authorName, Validators.required],
+        category: [this.currentMusic.category, Validators.required],
+        imageUrl: [this.currentMusic.imageUrl, Validators.required]
+      })
+      this.title='Edit '+this.currentMusic.albumName;
+    }
+    else{
+      this.title='Add Music'
     }
   }
 
-  public onSubmit(){
-    this.musicService.addMusic(this.music);
-    this.music={
-      albumName:"",
-      authorName:"",
-      category:"",
-      imageUrl:""
+  public onSubmit() {
+    if (this.musicProduct.valid) {
+      console.log(this.musicProduct.controls['id']);
+      this.validationCheck=false;
+      if (this.musicProduct.controls['id'].value != null) {
+        console.log(this.musicProduct.value);
+        this.musicService.updateMusic(this.musicProduct.value as IMusic);
+        this.reset();
+        this.router.navigate(['bodyContainer']);
+      }
+      else {
+        this.musicService.addMusic(this.musicProduct.value as Partial<IMusic>)
+      }
+      this.reset();
+      this.resetFormControls();
+    }
+    else {
+      this.validationCheck = true;
     }
   }
-  public onVisit(){
+
+
+  public onVisit() {
     this.router.navigate(['bodyContainer'])
+  }
+
+  public resetFormControls() {
+    Object.keys(this.musicProduct.controls).forEach((key) => {
+      const control = this.musicProduct.get(key);
+      if (control) {
+        control.markAsUntouched(); // Mark control as untouched
+        control.markAsPristine();  // Mark control as pristine (no changes)
+      }
+    });
+
+    // Trigger change detection to update the view
+    this.cdr.detectChanges();
+  }
+
+  public reset() {
+    this.musicProduct.patchValue({
+      albumName: '',
+      authorName: '',
+      category: '',
+      imageUrl: ''
+    })
   }
 }
